122c5ceff7fc09a100b9aeed2d62d6cc8049cca7
Kaloyan <kthristow@gmail.com>
2024-02-04 20:49:21 +0200

    Add package for using the camera device
    Add package for using the camera device and taking picture.

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,9 +1,45 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, ScrollView, TextInput, StyleSheet } from "react-native";
+import { Colors } from "../../constants/styles";
+import ImagePicker from "./ImagePicker";
 
-function PlaceForm(){
-    return <View>
-        <Text>PlaceForm</Text>
-    </View>
+function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function titleChangeHandler(text) {
+    setEnteredTitle(text);
+  }
+
+  return (
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput style={styles.input} onChangeText={titleChangeHandler} value={enteredTitle} />
+      </View>
+      <ImagePicker />
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 15,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
 
 export default PlaceForm;
